49218d7a13db224423437d7b0a7a3289f0b3579c
Debiprasad <debiprasad.dash@nettantra.net>
2022-04-11 14:54:26 +0530

    product update added

--- shoppingCartApi/routes/product.js
+++ shoppingCartApi/routes/product.js
@@ -22,7 +22,7 @@ const upload = multer({
 })
 
 
-router.post('/',verify, upload.single('image'), async (req, res) => {
+router.post('/', verify, upload.single('image'), async (req, res) => {
 
     const image = req.file.path;
     const uploadResponse = await cloudinary.uploader.upload(image, {
@@ -37,18 +37,18 @@ router.post('/',verify, upload.single('image'), async (req, res) => {
     try {
         let Product = await product.save();
         let category = await Category.findOne({ _id: req.body.category })
-        if(category){
+        if (category) {
             if (category.product.includes(Product._id)) {
-            return res.status(200).json(Product);
+                return res.status(200).json(Product);
 
-        } else {
-            category.product.push(Product._id)
-            let updatedCategory = await Category.findByIdAndUpdate(req.body.category, category)
-            if (updatedCategory) {
-                return res.status(200).json(Product)
+            } else {
+                category.product.push(Product._id)
+                let updatedCategory = await Category.findByIdAndUpdate(req.body.category, category)
+                if (updatedCategory) {
+                    return res.status(200).json(Product)
+                }
             }
-        }
-    }else {return res.status(400).send("sorry .something error!")}
+        } else { return res.status(400).send("sorry .something error!") }
 
     } catch (err) {
         return res.status(400).send(err)
@@ -57,16 +57,49 @@ router.post('/',verify, upload.single('image'), async (req, res) => {
 })
 
 
-router.get('/',verify, async (req, res) => {
+router.get('/', verify, async (req, res) => {
     try {
-        await Product.find().populate({path:'category',select:['category']}).exec(
+        await Product.find().populate({ path: 'category', select: ['category'] }).exec(
             (err, product) => {
-            if (err) res.status(400).send(err)
-            else return res.status(200).json(product)
-        })
+                if (err) res.status(400).send(err)
+                else return res.status(200).json(product)
+            })
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
 })
 
+router.put('/:id',upload.single('image'), async (req, res) => {
+    const image = req.file.path;
+    const uploadResponse = await cloudinary.uploader.upload(image, {
+        upload_preset: 'dev_setup',
+    });
+    try {
+        await Product.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            price: req.body.price,
+            image:uploadResponse.url
+        }, { new: true }).then(product => {
+            if (!product) {
+                return res.status(404).send({
+                    message: "product not found with id " + req.params.id
+                });
+            }
+            res.send(product);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "product not found with id " + req.params.id
+                });
+            }
+            return res.status(500).send({
+                message: "Error updating product with id " + req.params.id
+            });
+        });
+
+
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 module.exports = router;
